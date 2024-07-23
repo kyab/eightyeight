667f739e5301b8ec542534f5ee37ad717c8494b5
KojiYoshioka <kyossi212@gmail.com>
2024-07-23 05:30:08 +0000

    blah

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/location")
+async def location(lon: float):
+    print("lon = ", lon)
+    return {"lon" : lon}
+
