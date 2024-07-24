748de8c9fbd4cd6aed60bde96845a2104c6ac743
KojiYoshioka <kyossi212@gmail.com>
2024-07-24 11:16:38 +0000

    add site association

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/.well-known", StaticFiles(directory=".well-known"), name="well-known")
 
 @app.get("/")
 async def root():
@@ -13,3 +15,6 @@ async def location(lon: float):
     print("lon = ", lon)
     return {"lon" : lon}
 
+@app.get("/appclip")
+async def appclip():
+    return {"message" : "Hello AppClip"}
